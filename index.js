1bfd8806b3557c831e683a6d72d3063fbcd5a5dd
Henrique Tabelini <henriquetabelini@gmail.com>
2022-12-23 01:20:08 +0000

    Versão: 09 - Comandos de Repetição

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-const newLocal = require('prompt-sync')();
-const prompt = newLocal;
-var nome = prompt('Digite o seu nome:');
-var salario = parseFloat(prompt('Informe o seu salário bruto:'));
-var dependentes = parseInt(prompt('Digite o número de dependentes:') + 1);
-var rp = salario / dependentes;
-if (rp >= 500) {
-  if (salario <= 1903.98) {
-    var imposto = salario * 0.05;
-  } else if (salario <= 2826.65) {
-    var imposto = salario * 0.075;
+for (i = 1; i <= 5; i++) {
+  console.log("Pessoa ", i)
+  const newLocal = require('prompt-sync')();
+  const prompt = newLocal;
+  var nome = prompt('Digite o seu nome:');
+  var salario = parseFloat(prompt('Informe o seu salário bruto:'));
+  var dependentes = parseInt(prompt('Digite o número de dependentes:') + 1);
+  var rp = salario / dependentes;
+  if (rp >= 500) {
+    if (salario <= 1903.98) {
+      var imposto = salario * 0.05;
+    } else if (salario <= 2826.65) {
+      var imposto = salario * 0.075;
+    } else {
+      var imposto = salario * 0.15;
+    }
   } else {
-    var imposto = salario * 0.15;
+    var imposto = 0;
   }
-} else {
-  var imposto = 0;
+  var salario_liq = salario - imposto;
+  console.log(nome, "seu salário líquido é R$ ", salario_liq, ".");
 }
-var salario_liq = salario - imposto;
-console.log(nome, "seu salário líquido é R$ ", salario_liq, ".");
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 Problema:
 
-Elabore um algoritmo para calcular o salário líquido de uma pessoa.
+Elabore um algoritmo para calcular o salário líquido de cinco pessoas.
 Solicite ao usuário que digite seu nome e o valor de seu salário bruto.
 Peça também, para o usuário digitar a quantidade de dependentes.
 Calcule a renda familiar per capta.
@@ -12,4 +12,4 @@ Caso a renda para cada membro da família seja maior ou igual a R$ 500,00 o impo
 
 Ao final, mostre na tela o valor do salário líquido.
 
-Versão: 08 - Comandos de Decisão Aninhados
+Versão: 09 - Comandos de Repetição
